a414d1b79f770ce114115ecd6e9b3528705e0a71
nadaikasari <nadaikasari01@gmail.com>
2022-04-14 09:46:15 +0000

    Create and migrate model Category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_14_092614) do
+ActiveRecord::Schema.define(version: 2022_04_14_094439) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "menus", force: :cascade do |t|
     t.string "name"
