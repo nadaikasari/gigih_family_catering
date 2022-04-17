3b1380be35811266900664694c65f43d06d2d45b
nadaikasari <nadaikasari01@gmail.com>
2022-04-17 05:24:05 +0000

    Add function before action, set_menu and params to menus_controller

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,4 +1,7 @@
 class MenusController < ApplicationController
+
+  before_action :set_menu, only: [:show, :edit, :update, :destroy ]
+  
   def index
     @menus = params[:letter].nil? ? Menu.all : Menu.by_letter(params[:letter])
   end
@@ -10,5 +13,14 @@ class MenusController < ApplicationController
   end
 
   def edit
+  end
+
+  private
+  def set_menu
+    @menu = Menu.find(params[:id])
+  end
+  
+  def menu_params
+    params.require(:menu).permit(:name)
   end
 end
