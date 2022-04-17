d6a45130273e200116f2b1a750e419aeb3a74897
nadaikasari <nadaikasari01@gmail.com>
2022-04-17 12:54:26 +0000

    Add validation for customer is valid with name, email and phone

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -4,5 +4,10 @@ RSpec.describe Customer, type: :model do
   it'should have_many :order' do
       expect(Customer.reflect_on_association(:orders).macro).to eq :has_many
   end
+
+  it 'is valid with a name, email and phone' do
+    expect(FactoryBot.build(:customer)).to be_valid
+  end
+
   
 end
