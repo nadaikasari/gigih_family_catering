0d89118daf5baa38d05b7842054835c00ed7f177
nadaikasari <nadaikasari01@gmail.com>
2022-04-17 03:23:43 +0000

    Add validation for Menu, should have database realation with order details

--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Menu, type: :model do
   it'should have_many :item_categories' do
       expect(Menu.reflect_on_association(:item_categories).macro).to eq :has_many
   end
+
+  it'should have_many :order_details' do
+      expect(Menu.reflect_on_association(:order_details).macro).to eq :has_many
+  end
 end
