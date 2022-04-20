4764e9d952170723b291494e1e4448c4a50a9dc2
Nada Ika Sari <nadaikasari01@gmail.com>
2022-04-20 19:01:27 +0700

    Create factory bot value for order detail

--- db/migrate/20220414124741_create_order_details.rb
+++ db/migrate/20220414124741_create_order_details.rb
@@ -1,8 +1,8 @@
 class CreateOrderDetails < ActiveRecord::Migration[7.0]
   def change
     create_table :order_details do |t|
-      t.string :order_id
-      t.string :menu_id
+      t.integer :order_id
+      t.integer :menu_id
       t.float :price
       t.integer :quantity
         
--- spec/factories/order_details.rb
+++ spec/factories/order_details.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :order_detail do
-    
+    order_id { 1 }
+    menu_id { 1 }
+    price { 20000 }
+    quantity { 2 }
+  end
+
+  factory :invalid_order_detail, parent: :order_detail do
+    order_id { nil }
+    menu_id { nil }
+    price { "seribu" }
+    quantity { nil }
   end
 end
