ee7bcac98b1c638764f3e0eaff20075da67e504c
Nada Ika Sari <nadaikasari01@gmail.com>
2022-04-20 21:37:42 +0700

    Fixed validation model menu should have relation with order detail

--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe Menu, type: :model do
     expect(Menu.reflect_on_association(:item_categories).macro).to eq :has_many
   end
 
-  it'should have_many :menus' do
-    expect(Menu.reflect_on_association(:menus).macro).to eq :has_many
+  it'should have_many :order_details' do
+    expect(Menu.reflect_on_association(:order_details).macro).to eq :has_many
   end
 
   it 'is valid with a name, description and price' do
