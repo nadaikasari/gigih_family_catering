1c13ead6331516aaba69d50824857136f3a6feca
Nada Ika Sari <nadaikasari01@gmail.com>
2022-04-20 11:47:47 +0700

    Add item category rspec method

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_14_124741) do
+ActiveRecord::Schema.define(version: 2022_04_20_043322) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
--- spec/models/item_category_spec.rb
+++ spec/models/item_category_spec.rb
@@ -3,3 +3,8 @@ require 'rails_helper'
 RSpec.describe ItemCategory, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
+require 'rails_helper'
+
+RSpec.describe ItemCategory, type: :model do
+
+end
