0004dd46a165bf85657ad78e2c6ee6e8a41780c7
Nada Ika Sari <nadaikasari01@gmail.com>
2022-04-20 16:58:31 +0700

    Add validation for order at show method

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
 
 describe OrdersController do
+
+    before :each do
+        Customer.create(name: "Nadiya", email: "nadiya@gmail.com")
+    end
+
     describe 'GET #index' do
         it "populates an array of all Orders" do 
-            Customer.create(name: "Nadiya", email: "nadiya@gmail.com")
             order1 = create(:order, customer_id: 1, order_date: "2022/04/14", status: "NEW")
             order2 = create(:order, customer_id: 1, order_date: "2022/04/13", status: "NEW")
             get :index
@@ -13,6 +17,20 @@ describe OrdersController do
         it "renders the :index template" do
             get :index
             expect(response).to render_template :index
+        end
+    end
+
+    describe 'GET #show', :show do
+        it "assigns the requested order to @order" do
+            order = create(:order)
+            get :show, params: { id: order }
+            expect(assigns(:order)).to eq order
+        end
+
+        it "renders the :show template" do
+            order = create(:order)
+            get :show, params: { id: order }
+            expect(response).to render_template :show
         end
     end
 end
