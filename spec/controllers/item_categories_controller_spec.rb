f097cbfd9b191ce4cc4e212f92d4a789a4f809b2
Nada Ika Sari <nadaikasari01@gmail.com>
2022-04-20 12:39:57 +0700

    Add validation for food has a relation belongs to menu

--- spec/controllers/item_categories_controller_spec.rb
+++ spec/controllers/item_categories_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe ItemCategoryController do
+describe ItemCategoriesController do
 
 
   
--- spec/models/item_category_spec.rb
+++ spec/models/item_category_spec.rb
@@ -1,10 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe ItemCategory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
-require 'rails_helper'
-
-RSpec.describe ItemCategory, type: :model do
-
+  it'should belongs_to :menu' do
+    expect(ItemCategory.reflect_on_association(:menu).macro).to eq :belongs_to
+  end
 end
