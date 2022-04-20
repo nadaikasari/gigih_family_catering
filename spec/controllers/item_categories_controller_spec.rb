fb25d8e0141f2c3c20cd64c57bf67482c4cb3221
Nada Ika Sari <nadaikasari01@gmail.com>
2022-04-20 18:25:40 +0700

    Add validation for item category at new method

--- spec/controllers/item_categories_controller_spec.rb
+++ spec/controllers/item_categories_controller_spec.rb
@@ -34,4 +34,18 @@ describe ItemCategoriesController do
             expect(response).to render_template :show
         end
     end
+
+    describe 'GET #new' do
+        it "assigns a new ItemCategory to @item_category" do
+            get :new
+            expect(assigns(:item_category)).to be_a_new(ItemCategory)
+        end
+
+        it "renders the :new template" do
+            get :new
+            expect(:response).to render_template :new
+        end
+    end
+
+
 end
