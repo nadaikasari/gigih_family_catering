b3ba0052f67a3cda14e5cf85edfda2fdcbc01b6a
Nada Ika Sari <nadaikasari01@gmail.com>
2022-04-20 19:33:42 +0700

    Add validation for order details is valid with order_id, , menu_id, price and quantity

--- spec/models/order_detail_spec.rb
+++ spec/models/order_detail_spec.rb
@@ -16,4 +16,9 @@ RSpec.describe OrderDetail, type: :model do
     expect(OrderDetail.reflect_on_association(:order).macro).to eq :belongs_to
   end
 
+  it 'is valid with order_id, menu_id, price and quantity' do
+    expect(FactoryBot.build(:order_detail)).to be_valid
+  end
+
+
 end
