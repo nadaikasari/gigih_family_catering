f83e61b19db32c75108e5d3cd0491605508075ee
Nada Ika Sari <nadaikasari01@gmail.com>
2022-04-21 07:32:54 +0700

    Fixed validation method index in categories_controller

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -3,32 +3,16 @@ require 'rails_helper'
 describe CategoriesController do
   
   describe 'GET #index' do
-    context 'with params[:letter]' do
-      it "populates an array of categories starting with the letter" do
-        dessert = create(:category, name: "Dessert")
-        beverage = create(:category, name: "Beverage")
-        get :index, params: { letter: 'D' }
-        expect(assigns(:categories)).to match_array([dessert])
-      end
-
-      it "renders the :index template" do
-        get :index, params: { letter: 'D' }
-        expect(response).to render_template :index
-      end
+    it "populates an array of all categories" do 
+      dessert = create(:category, name: "Dessert")
+      beverage = create(:category, name: "Beverage")
+      get :index
+      expect(assigns(:categories)).to match_array([dessert, beverage])
     end
 
-    context 'without params[:letter]' do
-      it "populates an array of all categories" do 
-        dessert = create(:category, name: "Dessert")
-        beverage = create(:category, name: "Beverage")
-        get :index
-        expect(assigns(:categories)).to match_array([dessert, beverage])
-      end
-
-      it "renders the :index template" do
-        get :index
-        expect(response).to render_template :index
-      end
+    it "renders the :index template" do
+      get :index
+      expect(response).to render_template :index
     end
   end
   
