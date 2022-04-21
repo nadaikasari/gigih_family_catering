f26a2d1d54b96aede36c8407d78a9190e2617aff
Nada Ika Sari <nadaikasari01@gmail.com>
2022-04-21 07:34:20 +0700

    Delete validation find by letter in category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,9 +2,5 @@ class Category < ApplicationRecord
   has_many :item_categories
 
   validates :name, presence: true, uniqueness: true
-
-  def self.by_letter(letter)
-    where("name LIKE ?", "#{letter}%").order(:name)
-  end
   
 end
